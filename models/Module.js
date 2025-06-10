183a98639b0a0ffbe2beaca6a2cd66abf980383f
fatima1ahmad <fatimalafi009@gmail.com>
2025-06-10 18:53:21 +0300

    updates

--- models/Module.js
+++ models/Module.js
@@ -1,17 +1,17 @@
-import { query } from "../config/db.js";
+import pool from "../config/db.js";
 
 const ModuleModel = {
   async create({ course_id, title, description, order, duration }) {
-    const { rows } = await query(
+    const { rows } = await pool.query(
       `INSERT INTO modules (course_id, title, description, "order", duration)  
-       VALUES ($1, $2, $3, $4,$5) RETURNING *`,
+       VALUES ($1, $2, $3, $4, $5) RETURNING *`,
       [course_id, title, description, order, duration]
     );
     return rows[0];
   },
 
   async findByCourseId(courseId) {
-    const { rows } = await query(
+    const { rows } = await pool.query(
       `SELECT * FROM modules WHERE course_id = $1 ORDER BY "order"`,
       [courseId]
     );
@@ -19,17 +19,19 @@ const ModuleModel = {
   },
 
   async findById(id) {
-    const { rows } = await query(`SELECT * FROM modules WHERE id = $1`, [id]);
+    const { rows } = await pool.query(`SELECT * FROM modules WHERE id = $1`, [
+      id,
+    ]);
     return rows[0];
   },
 
   async update(id, { title, description, order, duration }) {
-    const { rows } = await query(
+    const { rows } = await pool.query(
       `UPDATE modules 
        SET title = COALESCE($1, title),
            description = COALESCE($2, description),
            "order" = COALESCE($3, "order"),
-          duration = COALESCE($4, duration),
+           duration = COALESCE($4, duration),
            updated_at = NOW()
        WHERE id = $5
        RETURNING *`,
@@ -39,7 +41,7 @@ const ModuleModel = {
   },
 
   async delete(id) {
-    await query(`DELETE FROM modules WHERE id = $1`, [id]);
+    await pool.query(`DELETE FROM modules WHERE id = $1`, [id]);
     return true;
   },
 };
